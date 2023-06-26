69c743654a65c8b1e347107ec23d92b8513ae628
abdullinda <105074853+abdullinda@users.noreply.github.com>
2023-06-27 02:11:14 +0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,9 @@
+import { MongoClient } from "mongodb"
+
+const client = new MongoClient("mongodb://127.0.0.1:27017/")
+
+client.connect()
+    .then(function (conection) {
+        const db = conection.db("AH20231CP1")
+        db.collection("Projects").insertOne({ name: "Esto es desde Node!" })
+    })
